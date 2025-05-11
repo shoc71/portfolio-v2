cf0cac7b5bed3106e9295f670636fd4b161867f7
shoc71 <supersonicwind6@gmail.com>
2025-05-11 02:09:26 -0400

    client init

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 const app = express();
@@ -8,13 +10,16 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(cors());
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // routes
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/client/dist")));
+    app.use(express.static(path.join(__dirname, "../client/.next")));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+        res.sendFile(path.resolve(__dirname, "../ client /.next / server / pages / index.html"));
     });
 }
 
