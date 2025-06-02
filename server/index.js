10f2d59cdad471973059c0723dadee2e95476801
Unos <129419169+shoc71@users.noreply.github.com>
2025-06-01 21:25:34 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
-// import mailRoutes from './routes/mailExport';
 
 dotenv.config();
 const app = express();
@@ -17,7 +16,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // API Routes
-// app.use("/api", mailRoutes);
 
 // Serve frontend in production
 if (process.env.NODE_ENV === "production") {
