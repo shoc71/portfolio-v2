aa5b171de27d31d7a3cecf7831b9a26013d9d725
Unos <129419169+shoc71@users.noreply.github.com>
2025-06-01 20:57:39 -0400

    small correction for mailRoutes import

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
-// import mailRoutes from './routes/mailExport';
+import mailRoutes from './routes/mailExport';
 
 dotenv.config();
 const app = express();
