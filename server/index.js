05114acbb86bf173e4807f36fba1ccf37a2235f6
Unos <129419169+shoc71@users.noreply.github.com>
2025-06-04 23:30:15 -0400

    kasdaoskd

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import path from "path";
-import { fileURLToPath } from "url";
+// import path from "path";
+// import { fileURLToPath } from "url";
 
 dotenv.config();
 const app = express();
@@ -12,20 +12,20 @@ app.use(express.json());
 app.use(cors());
 
 // __dirname workaround for ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 // API Routes
 
 // Serve frontend in production
-if (process.env.NODE_ENV === "production") {
-    const clientPath = path.join(__dirname, "../client/out");
-    app.use(express.static(clientPath));
+// if (process.env.NODE_ENV === "production") {
+//     const clientPath = path.join(__dirname, "../client/out");
+//     app.use(express.static(clientPath));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(clientPath, "index.html"));
-    });
-}
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.join(clientPath, "index.html"));
+//     });
+// }
 
 app.listen(PORT, () => {
     console.log(`✅ Server running on http://localhost:${PORT}`);
