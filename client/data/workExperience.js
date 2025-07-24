433897c4465baa62873493085b5262cd531d213e
Unos <129419169+shoc71@users.noreply.github.com>
2025-07-23 22:14:57 -0400

    updated work-exp

--- client/data/workExperience.js
+++ client/data/workExperience.js
@@ -2,25 +2,26 @@ export const workExperience = [
     
     {
         jobTitle: "Software Developer",
-        companyName: "Totally Legit Freelancing Coders Company",
+        companyName: "Coretech Infrastructure Solutions Inc.",
         startMonth: "Jun",
-        startYear: "2024",
+        startYear: "2025",
         endMonth: "Present",
         endYear: "",
         current: true,
         description:
             [
-                "Freelancing Family/Friend Requests (Charges vary on how much I liked them)",
-                "Ensured designs are in compliance with specifications",
-                "Constant Bug-Montioring and Testing After Delivery to ensure Client Satisifaction (iT's A fEaTuRe...)",
-                "Code Works + Extensive Documentation (It works, it works... the comments are there to make sure you know that it works)",
-                "Shoot me an email. I love talking to online strangers."
+                "Design, Debug, Develop, and Maintain Testable Responsive Web Applications",
+                "Created (unit) test cases for specific code features, functionalities, API calls, and server/cloud requests to ensure they meet quality standards",
+                "Efficient use of Git Branches for version control and monitoring any cloud errors when implentations procedure is followed",
+                "Reading and Editing Documentation and Comments for codebase based on supervisor recommendations",
+                "Ensuring that back-up server(s) procedures and documentation are up-to-date.",
+                "Maintain and deploy applications in cloud environment using AWS Technology"
             ],
     },
-    
+
     // {
-    //     jobTitle: "Jr. Frontend Developer",
-    //     companyName: "AvantGarde Tek Inc.",
+    //     jobTitle: "Software Developer",
+    //     companyName: "Totall Legit Company and Filler text ehre",
     //     startMonth: "Jun",
     //     startYear: "2024",
     //     endMonth: "Present",
@@ -28,11 +29,11 @@ export const workExperience = [
     //     current: true,
     //     description:
     //         [
-    //             "Effectively collaborate with business analysts, quality assurance analysts, and DBA's throughout the software development life cycle",
-    //             "Analyze, debug and resolve defects",
-    //             "Review, test and document software functionality",
-    //             "Write well designed, testable, efficient code",
-    //             "Ensure designs are in compliance with specifications"
+    //             "Freelancing Family/Friend Requests (Charges vary on how much I liked them)",
+    //             "Ensured designs are in compliance with specifications",
+    //             "Constant Bug-Montioring and Testing After Delivery to ensure Client Satisifaction (iT's A fEaTuRe...)",
+    //             "Code Works + Extensive Documentation (It works, it works... the comments are there to make sure you know that it works)",
+    //             "Shoot me an email. I love talking to online strangers."
     //         ],
-    // }
+    // },
 ];
